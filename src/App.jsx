25bdb18548d98f8fe0fc019cc0441e5077aa0cb2
HanJuYoung309 <juyg407@naver.com>
2025-11-01 00:54:45 +0900

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="text-center mt-20">
+      <h1 className="text-3xl font-bold text-gray-800">
+        🎉 React 프로젝트 세팅 완료!
+      </h1>
+      <p className="text-gray-600 mt-2">
+        이제 여기에 원하는 컴포넌트를 추가해보세요.
+      </p>
+    </div>
+  );
+}
+
+export default App;
